46292e59cd872db63c34e0985b33dcab068dea88
xiehou3131 <xiehou@sjtu.edu.cn>
2021-08-02 02:07:25 +0800

    finish history trades crawler

--- Modules/history_crawler.py
+++ Modules/history_crawler.py
@@ -3,6 +3,7 @@ import json
 import time
 import datetime
 import pytz
+from datetime import timezone
 
 import requests
 from .mysql_connector import CryptoCoinConnector
@@ -103,16 +104,21 @@ class BaseExchangeHistoryCrawler(BaseCrawler):
             while True:
                 print_write_data()
                 if i % self.rate_limit == 0:
-                    time.sleep(self.interval)
                     print_sleep(self.interval)
+                    time.sleep(self.interval)
                 
             
-                try:
-                    start_timestamp, end_timestamp, size = self._kernel(symbol, start_timestamp, end_timestamp)
-                    if size == 0:
-                        break
-                except:
-                    self.handle_network_issue(self._kernel, symbol)
+                start_timestamp, end_timestamp, size = self._kernel(symbol, start_timestamp, end_timestamp)
+                if start_timestamp > end_timestamp:
+                    break
+                if size == 0:
+                    break
+                # try:
+                #     start_timestamp, end_timestamp, size = self._kernel(symbol, start_timestamp, end_timestamp)
+                #     if size == 0:
+                #         break
+                # except:
+                #     self.handle_network_issue(self._kernel, symbol)
                 
                 if test_flag:
                     break
@@ -125,11 +131,12 @@ class FTXUSHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
     def __init__(self, db_name, symbols):
         super(FTXUSHistoryTradeDataCrawler, self).__init__(db_name, symbols, "ftxus")
         self.url = "https://ftx.us/api/markets/{}/trades?start_time={}&end_time={}"
-        self.rate_limit = 20
+        self.rate_limit = 29
         self.interval = 1
-    
+
     def transform_symbol(self, symbol):
-        return symbol[:-4] + "/" + "USD"
+        pair = symbol.split("_")
+        return pair[0] + "/" + pair[1]
 
     def parse_data(self, res_data, start_timestamp, end_timestamp):
         data_lst = []
@@ -139,10 +146,10 @@ class FTXUSHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
         for record in res_data["result"]:
             data = [self.exch_name]
             isBuyerMaker = True if record["side"] == "buy" else False
-            quoteQty = record["price"] * record["size"]
+            quoteQty = float(record["price"]) * float(record["size"])
             time = record["time"][0:19]
             d = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
-            ts = int(d.timestamp())
+            ts = int(d.replace(tzinfo=timezone.utc).timestamp())
             data += [record["id"], record["price"], record["size"], quoteQty, ts, isBuyerMaker]
             data_lst.append(data)
 
@@ -151,6 +158,229 @@ class FTXUSHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
             # find the smallest timestamp to be the new end_timestamp
             new_end_time = res_data["result"][-1]["time"][0:19]
             d = datetime.datetime.strptime(new_end_time, "%Y-%m-%dT%H:%M:%S")
-            end_timestamp = int(d.timestamp())
+            end_timestamp = int(d.replace(tzinfo=timezone.utc).timestamp())
+
+        return data_lst, start_timestamp, end_timestamp, size
+
+class FTXHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
+    def __init__(self, db_name, symbols):
+        super(FTXHistoryTradeDataCrawler, self).__init__(db_name, symbols, "ftx")
+        self.url = "https://ftx.com/api/markets/{}/trades?start_time={}&end_time={}"
+        self.rate_limit = 29
+        self.interval = 1
+    
+    def transform_symbol(self, symbol):
+        pair = symbol.split("_")
+        return pair[0] + "/" + pair[1]
+
+    def parse_data(self, res_data, start_timestamp, end_timestamp):
+        data_lst = []
+        if res_data["success"] != True:
+            return data_lst, start_timestamp, end_timestamp, 0
+
+        for record in res_data["result"]:
+            data = [self.exch_name]
+            isBuyerMaker = True if record["side"] == "buy" else False
+            quoteQty = float(record["price"]) * float(record["size"])
+            time = record["time"][0:19]
+            d = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
+            ts = int(d.replace(tzinfo=timezone.utc).timestamp())
+            data += [record["id"], record["price"], record["size"], quoteQty, ts, isBuyerMaker]
+            data_lst.append(data)
+
+        size = len(res_data["result"])
+        if size != 0:
+            # find the smallest timestamp to be the new end_timestamp
+            new_end_time = res_data["result"][-1]["time"][0:19]
+            d = datetime.datetime.strptime(new_end_time, "%Y-%m-%dT%H:%M:%S")
+            end_timestamp = int(d.replace(tzinfo=timezone.utc).timestamp())
+
+        return data_lst, start_timestamp, end_timestamp, size
+
+class LiquidHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
+    def __init__(self, db_name, symbols):
+        super(LiquidHistoryTradeDataCrawler, self).__init__(db_name, symbols, "liquid")
+        self.url = "https://api.liquid.com/executions?product_id={}&timestamp={}&limit=100"
+        self.url_symbol_to_id = "https://api.liquid.com/products"
+        self.rate_limit = 200
+        self.interval = 300
+    
+    def transform_symbol(self, symbol):
+        url = self.url_symbol_to_id
+        response = requests.get(url)
+        res_data = json.loads(response.text)
+        product_id = -1
+
+        pair = symbol.split("_")
+        symbol = pair[0] + pair[1] # reconstruct from BTC_USDT to BTCUSDT
+        
+        for record in res_data:
+            if record["currency_pair_code"] == symbol:
+                product_id = record["id"]
+
+        return product_id
+
+    def construct_url(self, symbol, start_timestamp, end_timestamp):
+        return self.url.format(symbol, start_timestamp, end_timestamp)
+
+    def parse_data(self, res_data, start_timestamp, end_timestamp):
+        data_lst = []
+        if "message" in res_data and res_data["message"] == "Product not found":
+            return data_lst, start_timestamp, end_timestamp, 0
+        for record in res_data:
+            data = [self.exch_name]
+            isBuyerMaker = True if record["taker_side"] == "buy" else False
+            quoteQty = float(record["price"]) * float(record["quantity"])
+            data += [record["id"], record["price"], record["quantity"], quoteQty, record["created_at"], isBuyerMaker]
+            data_lst.append(data)
+
+        size = len(res_data)
+        if size != 0:
+            # find the smallest timestamp to be the new end_timestamp
+            start_timestamp = res_data[-1]["created_at"] + 1
+
+        return data_lst, start_timestamp, end_timestamp, size
+
+class BinanceHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
+    def __init__(self, db_name, symbols):
+        super(BinanceHistoryTradeDataCrawler, self).__init__(db_name, symbols, "binance")
+        self.url = "https://api1.binance.com/api/v3/klines?symbol={}&interval=1m&startTime={}&endTime={}"
+        self.rate_limit = 29
+        self.interval = 1
+
+    def transform_symbol(self, symbol):
+        pair = symbol.split("_")
+        return pair[0] + pair[1]
+
+    def construct_url(self, symbol, start_timestamp, end_timestamp):
+        return self.url.format(symbol, start_timestamp, end_timestamp)
+
+    def parse_data(self, res_data, start_timestamp, end_timestamp):
+        data_lst = []
+        if "message" in res_data:
+            return data_lst, start_timestamp, end_timestamp, 0
+
+        for record in res_data:
+            data = [self.exch_name]
+            isBuyerMaker = None
+            price = (float(record[2]) + float(record[3])) / 2
+            quantity = float(record[5])
+            quoteQty = price * quantity
+            opentime = int(record[0]) / 1000
+            id = str(record[0])
+            data += [id, price, quantity, quoteQty, opentime, isBuyerMaker]
+            data_lst.append(data)
+
+        size = len(res_data)
+        if size != 0:
+            # find the smallest timestamp to be the new end_timestamp
+            start_timestamp = res_data[-1][6]
+
+        return data_lst, start_timestamp, end_timestamp, size
+
+class KrakenHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
+    def __init__(self, db_name, symbols):
+        super(KrakenHistoryTradeDataCrawler, self).__init__(db_name, symbols, "kraken")
+        self.url = "https://api.kraken.com/0/public/Trades?pair={}&since={}"
+        self.rate_limit = 6
+        self.interval = 45
+    
+    def transform_symbol(self, symbol):
+        pair = symbol.split("_")
+        return pair[0] + pair[1]
+
+    def construct_url(self, symbol, start_timestamp, end_timestamp):
+        return self.url.format(symbol, start_timestamp, end_timestamp)
+
+    def parse_data(self, res_data, start_timestamp, end_timestamp):
+        data_lst = []
+        if len(res_data["error"]) > 0:
+            return data_lst, start_timestamp, end_timestamp, 0
+        trade_lst, _ = res_data["result"].values()
+        for record in trade_lst:
+            data = [self.exch_name]
+            price = float(record[0])
+            qty = float(record[1])
+            idx = 10000 * (float(record[0]) + float(record[2]))
+            tick = int(record[2])
+            quoteQty = price * qty
+            isBuyerMaker = True if record[3] == "b" else False
+            
+            data += [idx, price, qty, quoteQty, tick, isBuyerMaker]
+            data_lst.append(data)
+
+
+        size = len(data_lst)
+        if size != 0:
+            # find the smallest timestamp to be the new end_timestamp
+            start_timestamp = int(int(res_data["result"]["last"]) / 1000000000) + 1
+
+        return data_lst, start_timestamp, end_timestamp, size
+
+class BitfinexHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
+    def __init__(self, db_name, symbols):
+        super(BitfinexHistoryTradeDataCrawler, self).__init__(db_name, symbols, "bitfinex")
+        self.url = "https://api-pub.bitfinex.com/v2/trades/{}/hist?limit=10000&start={}&end={}"
+        self.rate_limit = 90
+        self.interval = 60
+    
+    def transform_symbol(self, symbol):
+        pair = symbol.split("_")
+        return "t" + pair[0] + "USD"
+
+    def construct_url(self, symbol, start_timestamp, end_timestamp):
+        return self.url.format(symbol, start_timestamp * 1000, end_timestamp * 1000)
+
+    def parse_data(self, res_data, start_timestamp, end_timestamp):
+        data_lst = []
+        if len(res_data) == 0:
+            return data_lst, start_timestamp, end_timestamp, 0
+        for record in res_data:
+            data = [self.exch_name]
+            tick = record[1] / 1000
+            qty = abs(record[2])
+            isBuyerMaker = True if record[2] > 0 else False
+            quoteQty = qty * record[3]
+            data += [record[0], record[3], qty, quoteQty, tick, isBuyerMaker]
+            data_lst.append(data)
+
+        size = len(data_lst)
+        if size != 0:
+            # find the smallest timestamp to be the new end_timestamp
+            end_timestamp = int(res_data[-1][1] / 1000) - 1
+
+        return data_lst, start_timestamp, end_timestamp, size
+
+class PoloniexHistoryTradeDataCrawler(BaseExchangeHistoryCrawler):
+    def __init__(self, db_name, symbols):
+        super(PoloniexHistoryTradeDataCrawler, self).__init__(db_name, symbols, "poloniex")
+        self.url = "https://api.gemini.com/v1/trades/{}?timestamp={}&limit_trades=1000"
+        self.rate_limit = 1
+        self.interval = 1
+    
+    def transform_symbol(self, symbol):
+        pair = symbol.split("_")
+        return pair[0]
+
+    def construct_url(self, symbol, start_timestamp, end_timestamp):
+        return self.url.format(symbol, start_timestamp * 1000, end_timestamp * 1000)
+
+    def parse_data(self, res_data, start_timestamp, end_timestamp):
+        data_lst = []
+        if len(res_data) == 0:
+            return data_lst, start_timestamp, end_timestamp, 0
+        for record in res_data:
+            data = [self.exch_name]
+            tick = record[1] / 1000
+            qty = abs(record[2])
+            isBuyerMaker = True if record[2] > 0 else False
+            quoteQty = qty * record[3]
+            data += [record[0], record[3], qty, quoteQty, tick, isBuyerMaker]
+            data_lst.append(data)
+
+        size = len(data_lst)
+        if size != 0:
+            # find the smallest timestamp to be the new end_timestamp
+            end_timestamp = int(res_data[-1][1] / 1000) - 1
 
         return data_lst, start_timestamp, end_timestamp, size
